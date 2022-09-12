897660c8fd075194fc4fd1590cde355e144e48d3
Amadi Y <absnacks17@gmail.com>
2022-09-12 12:04:24 -0500

    added the onload file

--- src/js-files/onPageLoad.js
+++ src/js-files/onPageLoad.js
@@ -2,8 +2,17 @@ const pageLoad = (() => {
     const container = document.querySelector(".container");
 
     container.innerHTML = `
-        <div class="item one">one</div>
-        <div class="item two">two</div>
+        <div class="item one">
+            <div>One</div>
+            <div>two</div>
+            <div>three</div>
+            <div>Add</div>
+        </div>
+        <div class="item two">
+            <div>Apple</div>
+            <div>Input field</div>
+            <div>Bananas</div>
+        </div>
     `;
 })();
 
