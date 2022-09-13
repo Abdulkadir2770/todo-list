0851183cf41d3a42b323d47d10446406943602a1
Amadi Y <absnacks17@gmail.com>
2022-09-13 11:01:46 -0500

    nothing

--- src/js-files/onPageLoad.js
+++ src/js-files/onPageLoad.js
@@ -2,15 +2,30 @@ const pageLoad = (() => {
     const container = document.querySelector(".container");
 
     container.innerHTML = `
-        <div class="item one">
-            <div>One</div>
-            <div>two</div>
-            <div>three</div>
-            <div>Add</div>
+        <div class="subContainerItem one">
+            <h1>Shopping List</h1>
+            <div>
+                <h3 class="items">General List</h3>
+                <br>
+                <h3 class="items">Completed</h3>
+                <br>
+                <h3 class="items">Trash</h3>
+            </div>
+            <div>
+                <h2>Projects</h2>
+            </div>
+            <div id="addProjectDiv">
+                <button id="addProject">+ Create Project</button>
+            </div>
         </div>
-        <div class="item two">
-            <div>Apple</div>
-            <div>Input field</div>
+        <div class="subContainerItem two">
+            <div>
+                <h2>General</list>
+            </div>
+            <div class="addTodoBtn">
+                <button id="allBtn">All</button>
+                <button id="createBtn">+ Create Todo</button>
+            </div>
             <div>Bananas</div>
         </div>
     `;
