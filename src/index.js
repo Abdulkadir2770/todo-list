06e4e57bbbc92007ed56d2011043a10c93785743
Amadi Y <absnacks17@gmail.com>
2022-09-17 10:35:56 -0500

    added clicking funtionality to the complete and trash tab

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ const pageContent = (() => {
     const container = document.querySelector(".container");
 
     // the two <div> items
+   const generalTab = document.querySelector(".general");
+   generalTab.addEventListener("click", () => {
+        console.log("General tab has been clicked");
+   })
 
+   const completedTab = document.querySelector(".completed");
+   completedTab.addEventListener("click", () => {
+        console.log("Completed tab has been clicked");
+   })
+
+   const trashTab = document.querySelector(".trash");
+   trashTab.addEventListener("click", () => {
+        console.log("Trash tab has been clicked");
+   })
 
 })();
--- src/js-files/onPageLoad.js
+++ src/js-files/onPageLoad.js
@@ -5,11 +5,11 @@ const pageLoad = (() => {
         <div class="subContainerItem one">
             <h1>Shopping List</h1>
             <div>
-                <h3 class="items">General List</h3>
+                <h3 class="items general">General List</h3>
                 <br>
-                <h3 class="items">Completed</h3>
+                <h3 class="items completed">Completed</h3>
                 <br>
-                <h3 class="items">Trash</h3>
+                <h3 class="items trash">Trash</h3>
             </div>
             <div>
                 <h2>Projects</h2>
